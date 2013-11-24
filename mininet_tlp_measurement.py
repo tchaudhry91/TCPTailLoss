1d92fafc5f18d15ab027ffe0746fccacaabe6dc4
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2013-11-24 15:15:11 +0100

    Testing

---
+++ mininet_tlp_measurement.py
@@ -0,0 +1,28 @@
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel 
+
+class BasicTopology(Topo):
+    def __init__(self, **opts):
+        #Initialise
+        Topo.__init__(self, **opts)
+        hostA = self.addHost('hostA')
+        hostB = self.addHost('hostB')
+        hostC = self.addHost('hostC')
+        self.addLink(hostA,hostB)
+        self.addLink(hostB,hostC)
+
+def simpleTest():
+    "Create and test a simple network"
+    topo = BasicTopology()
+    net = Mininet(topo)
+    net.start()
+    print "Dumping host Connections"
+    dumpNodeConnections(net.hosts)
+    net.pingAll()
+    net.stop
+
+if __name__=="__main__":
+    setLogLevel('info')
+    simpleTest()
