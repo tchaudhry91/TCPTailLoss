c591edb4a5e8b68aebc16ef8103e51518112beca
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2013-11-24 15:59:45 +0100

    Testing

--- mininet_tlp_measurement.py
+++ mininet_tlp_measurement.py
@@ -2,15 +2,21 @@ from mininet.topo import Topo
 from mininet.net import Mininet
 from mininet.util import dumpNodeConnections
 from mininet.log import setLogLevel 
+from mininet.node import Node
 
 class BasicTopology(Topo):
     def __init__(self, **opts):
         #Initialise
         Topo.__init__(self, **opts)
-        hostA = self.addHost('hostA')
-        hostA.seIP('10.0.0.1')
-        hostB = self.addHost('hostB')
-        hostC = self.addHost('hostC')
+        hostA = Node('hostA')
+        hostA.setIP('10.0.0.2')
+        hostB = Node('hostB')
+        hostB.setIP('10.0.0.1')
+        hostC = Node('hostC')
+        hostC.setIP('10.0.0.3')
+        self.addHost('hostA')
+        self.addHost('hostB')
+        self.addHost('hostC')
         self.addLink(hostA,hostB)
         self.addLink(hostB,hostC)
 
