f2fc6a54068c5765053dda1f31f5fa316d2d9f4e
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2013-11-25 01:27:24 +0100

    Added Command Line Argument Support

--- basic_topology.py
+++ basic_topology.py
@@ -3,20 +3,46 @@ from mininet.net import Mininet
 from mininet.node import Node
 
 class BasicTopology(Topo):
-    def __init__(self, **opts):
+    def __init__(self, configLink1, configLink2, **opts):
         #Initialise
+        if configLink1 == "fast":
+            self.bw = 1
+            self.delay = "5ms"
+        elif configLink1 == "moderate":
+            self.bw = 0.5
+            self.delay = "10ms"
+        elif configLink1 == "slow":
+            self.bw = 0.1
+            self.delay = "20ms"
+        
+        if configLink2 == "fast":
+            self.bw2 = 1 
+            self.delay2 = "5ms"
+        elif configLink2 == "moderate":
+            self.bw2 = 0.5
+            self.delay2 = "10ms"
+        elif configLink2 == "slow":
+            self.bw2 = 0.1
+            self.delay2 = "20ms"
+
+        self.max_queue_size = 1500
+
         Topo.__init__(self, **opts)
         hostA = self.addHost('hostA')
         hostB = self.addHost('hostB')
         hostC = self.addHost('hostC')
-        self.addLink(hostB,hostA)
-        self.addLink(hostB,hostC)
+        self.addLink(hostB,hostA, bw=self.bw,
+                        delay=self.delay, 
+                        max_queue_size=self.max_queue_size)
+        self.addLink(hostB,hostC, bw=self.bw2,
+                        delay=self.delay2,
+                        max_queue_size=self.max_queue_size)
 
 def configureHosts(net):
     for h in net.hosts:
         if h.name == "hostA":
             h.setIP(intf="hostA-eth0",ip="192.168.1.2/24")
-    	    h.cmdPrint("route add default gw 192.168.1.1")
+            h.cmdPrint("route add default gw 192.168.1.1")
         elif h.name == "hostB":
             h.setIP(intf="hostB-eth0", ip="192.168.1.1/24")
             h.setIP(intf="hostB-eth1", ip="192.168.2.1/24")
--- mininet_tlp_measurement.py
+++ mininet_tlp_measurement.py
@@ -2,15 +2,37 @@ from basic_topology import BasicTopology
 from basic_topology import configureHosts
 from mininet.net import Mininet
 
+import argparse
+
 def start():
     """
+        Initiate a MininetTLPMeasurementSession.
     """
-    topo = BasicTopology()
+    #ParseArguments
+    parser = argparse.ArgumentParser()
+    addArguments(parser)
+    args = parser.parse_args()
+
+    #Build Topology
+    topo = BasicTopology(args.ConfigLink1,
+                         args.ConfigLink2)
     net = Mininet(topo)
     configureHosts(net)
+    
+    #Start Network and Measurement
     net.start()
     net.pingAll()
     net.stop()
+
+
+def addArguments(parser):
+    parser.add_argument("ConfigLink1", 
+                        help=("The Configuration Of Link 1 -"+
+                              " Fast/Moderate/Slow"))
+    parser.add_argument("ConfigLink2",
+                        help=("The Configuration of Link 2 -"+
+                              " Fast/Moderate/Slow"))
+
 
 if __name__=="__main__":
     start()
