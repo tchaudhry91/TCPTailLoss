246fd8942ee5cb3b67db12b5ca6c0bc5c81bce50
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2013-11-25 15:42:11 +0100

    Fixed Concat Error

--- file_transfer.py
+++ file_transfer.py
@@ -7,11 +7,7 @@ def transferFileUsingNc6(hostA,hostC,transferSize):
         bytesT = (1500*128)-(18*128)
     elif transferSize == "long":
         bytesT = (1500*256)-(18*256)
-    hostA.cmd("dd if=/dev/zero of=temp count="+bytesT+" bs=1")
+    hostA.cmd("dd if=/dev/zero of=temp count="+str(bytesT)+" bs=1")
     hostA.cmd("nc6 -X -l -p 7676 < temp &")
     hostC.cmd("nc6 -X 192.168.1.2 7676 > /dev/null")
     hostA.cmd("rm temp")
-
-def transferFileUsingNc6(hostA,hostC):
-    print(hostA.cmd("nc6 -X -l -p 7676 < /dev/zero &"))
-    print(hostC.cmd("nc6 -X 192.168.1.2 7676 > target"))
