594ae16e513531b4ca0e58f5cde450c7d7b744c8
mininet <mininet@svm026.i1.as.net.in.tum.de>
2013-12-15 13:57:39 +0100

    Update

--- drop_tail.py
+++ drop_tail.py
@@ -2,15 +2,19 @@ import sys
 import nfqueue
 from socket import AF_INET
 
-payload_length = 0
-drop_count = 0
-
-def callback(i, payload):
+count = 0 
+def callback(handle, new_handle=None):
     """
         This function causes Tail Drop
     """
-    payload.set_verdict(nfqueue.NF_ACCEPT)
-    
+    global count
+    count = count + 1
+    if new_handle is not None:
+        handle = new_handle
+    if count > 10:
+        handle.set_verdict(nfqueue.NF_DROP)
+    else:
+        handle.set_verdict(nfqueue.NF_ACCEPT)
 
 if __name__=="__main__":
     global payload_length
