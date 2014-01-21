4fa02f9bc1b32c0e58a28313c799eae8de117f87
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2014-01-21 07:31:53 +0100

    Added More Options to Measurements

--- automatic_measurements.py
+++ automatic_measurements.py
@@ -1,7 +1,8 @@
 import subprocess
+import sys
 
 
-def measure(TLP_VALUE):
+def measure(TLP_VALUE, category_1, category_2):
     """Record the output for all the possible configurations.
         Each Analysis Output is saved in a separate file with
         10-15 entries for each configuration.
@@ -9,13 +10,33 @@ def measure(TLP_VALUE):
     subprocess.call(["sudo", "sysctl", "-w",
                     "net.ipv4.tcp_early_retrans="+TLP_VALUE])
     f_name = "Recordings"
+    link_speeds = []
+    payload_lengths = []
     if TLP_VALUE == "2":
         f_name += "/NoTLP"
     elif TLP_VALUE == "3":
         f_name = "/TLP"
-    link_speeds = ["fast", "moderate", "slow"]
-    payload_lengths = ["long", "medium", "short"]
+
+    if category_1 == "all":
+        link_speeds = ["fast", "moderate", "slow"]
+    elif category_1 == "fast":
+        link_speeds.append("fast")
+    elif category_1 == "moderate":
+        link_speeds.append("moderate")
+    elif category_1 == "slow":
+        link_speeds.append("slow")
+
+    if category_2 == "all":
+        payload_lengths = ["long", "medium", "short"]
+    elif category_2 == "long":
+        payload_lengths.append("long")
+    elif category_2 == "medium":
+        payload_lengths.append("medium")
+    elif category_2 == "short":
+        payload_lengths.append("short")
+
     drop_counts = [1, 2, 4, 8]
+
     for link_speed in link_speeds:
         for payload_length in payload_lengths:
             for drop_count in drop_counts:
@@ -41,5 +62,7 @@ def measure(TLP_VALUE):
 
 
 if __name__ == "__main__":
-    measure("2")
-    measure("3")
+    TLP = sys.argv[1]
+    category_1 = sys.argv[2]
+    category_2 = sys.argv[3]
+    measure(TLP, category_1, category_2)
